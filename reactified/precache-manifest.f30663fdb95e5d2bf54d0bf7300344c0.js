58ef7338b46c811d822469c4b914e2d23c35c194
Richard Arva <arvarichi@gmail.com>
2020-06-26 19:23:35 +0200

    reactified initial

---
+++ reactified/precache-manifest.f30663fdb95e5d2bf54d0bf7300344c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75ccb34c3d192d8d2a3ab6a10d9613d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "586179248d9f711f7ab3",
+    "url": "/static/css/main.757534f7.chunk.css"
+  },
+  {
+    "revision": "aac02bc217abb58444e3",
+    "url": "/static/js/2.fe6068a9.chunk.js"
+  },
+  {
+    "revision": "14b6951eb3599e7c836b2179fd9b2b8b",
+    "url": "/static/js/2.fe6068a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "586179248d9f711f7ab3",
+    "url": "/static/js/main.55aaf658.chunk.js"
+  },
+  {
+    "revision": "fb4b3e0d4feb8212ae17",
+    "url": "/static/js/runtime-main.55f9b93b.js"
+  }
+]);
